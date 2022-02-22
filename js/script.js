c6bf72a1c911270d1ade30b473d02a211f699dc4
unknown <netojose42@gmail.com>
2022-02-22 11:11:53 -0300

    implementado put e delete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-$.getJSON(`http://localhost:3000/pessoas`, function (data, status) {
+function listaPessoas(){
+$.getJSON(`https://api-flor.herokuapp.com/pessoas`, function (data, status) {
     const pessoas_db = []
 
     for (const pessoa of data) {
@@ -7,35 +8,87 @@ $.getJSON(`http://localhost:3000/pessoas`, function (data, status) {
             <td>${pessoa.id}</td>
             <td>${pessoa.nome}</td>
             <td>${pessoa.email}</td>
-            <td>${pessoa.telefone}</td>
+            <td>${pessoa.contato}</td>
             <td>${pessoa.cidade}</td>
             <td>
                 <button type="button" onclick="edit(${pessoa.id})" data-toggle="modal" data-target="#meuModal" class="btn btn-warning"><i class="bi bi-pencil"></i>Editar</button>
-                <button type="button"  class="btn btn-danger"><i class="bi bi-trash"></i></i>Excluir</button>
+                <button type="button"  onclick="excluiPessoa(${pessoa.id})" class="btn btn-danger"><i class="bi bi-trash"></i></i>Excluir</button>
             </td>
             </tR>
             `)
         $('#result').html(pessoas_db)
     }
 })
+}
 
+listaPessoas()
 function edit(id) {
-    $.getJSON(`http://localhost:3000/pessoas/${id}`, function (data, status) {
+    $.getJSON(`https://api-flor.herokuapp.com/pessoas/${id}`, function (data, status) {
+                $("#idUser").val(data.id)
                 $("#nome").val(data.nome)
                 $("#email").val(data.email)
-                $("#contato").val(data.telefone)
+                $("#contato").val(data.contato)
                 $("#cidade").val(data.cidade)
+                $("#salve").hide();
+                $("#btn-edit").show();
+                $("#titleCad").hide();
+                $("#titleEdit").show();
     });
 }
 
 $("#salve").click(function () {
-    $.post("http://localhost:3000/pessoas",
+    $.post("https://api-flor.herokuapp.com/pessoas",
         {
             "nome": $("#nome").val(),
             "email": $("#email").val(),
-            "telefone": $("#contato").val(),
+            "contato": $("#contato").val(),
             "cidade": $("#cidade").val()
         });
+        listaPessoas()
 });
 
+function limpaForm(){
+    $("#nome").val('')
+    $("#email").val('')
+    $("#contato").val('')
+    $("#cidade").val('')
+}
 
+
+function btnSave(){
+    $("#salve").show();
+    $("#btn-edit").hide();
+    $("#titleCad").show();
+    $("#titleEdit").hide();
+}
+
+function atualizarDados() {
+    var dados = $("input")
+    
+    dadosSalvar = {
+        nome: dados[1].value,
+        email: dados[2].value,
+        contato: dados[3].value,
+        cidade: dados[4].value
+    }
+
+    $.ajax({
+        type: "PUT",
+        url: `https://api-flor.herokuapp.com/pessoas/${dados[0].value}`,
+        data: dadosSalvar,
+        success: function (response) {
+            listaPessoas()
+        }
+    });
+}
+
+
+    function excluiPessoa(id) {
+        $.ajax({
+            type: "DELETE",
+            url: `https://api-flor.herokuapp.com/pessoas/${id}`,
+            success: function (response) {
+                listaPessoas()
+            }
+        });
+    }
